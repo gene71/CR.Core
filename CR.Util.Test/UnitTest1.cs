d743e9b23ed714db47ad61218459c9f57ba6a6bf
gene71 <gene.moore71@gmail.com>
2016-12-18 15:14:06 -0700

    init

---
+++ CR.Util.Test/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CR.Util.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string dir1 = @"C:\Working\Project\PTDS_Rework\Pshs 3.0.7";
+            string dir2 = @"C:\Working\Project\PTDS_Rework\WeatherPTDS_version_1.2.2";
+            BuildCheck dc = new BuildCheck();
+            Console.WriteLine(dc.HasBuildErrors(dir1));
+            
+            Console.WriteLine(dc.HasBuildErrors(dir2));
+            foreach(var e in dc.Errors)
+            {
+                Console.WriteLine(e);
+            }
+
+
+
+        }
+    }
+}
