d63649e2800c7711fbb04e3e4957412b3964b629
gene <gene@MAIN>
2016-12-28 13:10:46 -0700

    add CRLogger and Exception

---
+++ CR.Util/CRLogger.cs
@@ -0,0 +1,72 @@
+﻿using CR.Util.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CR.Util
+{
+    public class CRLogger
+    {
+
+        public enum CRLogTitle { Info, Error, Status };
+        private string logDir = "CodeReconLog.txt";
+        /// <summary>
+        /// WriteLog writes the specified  title and message to the logging directory. 
+        /// If a logging directory has not been set it writes to the default path.
+        /// The default path is the execution directory for the
+        /// hosting application.
+        /// </summary>
+        /// <param name="title"></param>
+        /// <param name="msg"></param>
+        public void WriteLog(CRLogTitle title, string msg)
+        {
+            try
+            {
+                using (FileStream fs = new FileStream(logDir, FileMode.Append, FileAccess.Write))
+                using (StreamWriter sw = new StreamWriter(fs))
+                {
+                    sw.WriteLine(DateTime.Now + " " + title + ": " + msg);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new CRUtilException(ex.Message);
+            }
+
+        }
+
+        /// <summary>
+        /// SetLogDir sets the logging directory for the logger.  If the
+        /// directoyr doesn't exist SetLogDir will attempt to create the directory
+        /// and set the path.  The default path is the execution directory for the
+        /// hosting application.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <exception cref="CRLoggerException">CRLoggerException</exception>
+        public void SetLogDir(string path)
+        {
+            try
+            {
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                    logDir = path + "\\" + logDir;
+
+                }
+                else
+                {
+                    logDir = path + "\\" + logDir;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new CRUtilException(ex.Message);
+            }
+
+
+        }
+    }
+}
---
+++ CR.Util/Exceptions/CRUtilException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CR.Util.Exceptions
+{
+    public class CRUtilException : Exception
+    {
+        public CRUtilException(string message) : base ("CRUtilException: " + message)
+        {
+
+        }
+    }
+}
